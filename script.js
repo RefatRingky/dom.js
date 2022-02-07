05e18662a389390e2aea61d9fd65036c7bb513fe
Refat Tamanna Ringky <95683348+RefatRingky@users.noreply.github.com>
2022-02-07 18:13:48 +0600

    dom setting commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// var friends=['rahim','karim','salim']
+// var i =0;
+// for(i = 0; i<friends.length;i++){
+//     console.log(friends[i]);
+// }
+// // for(const friend of friends){
+// //     console.log(friend);
+// // }
